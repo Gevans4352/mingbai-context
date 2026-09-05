1e05f22e0330ab2ad0a414c18be1397cd3404fed
Gevans4352 <emogoghena@gmail.com>
2026-09-05 16:06:28 +0100

    feat: exclude footer from home page, add privacy route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import Login from './pages/login';
-import Register from './pages/register';
-import History from './pages/history';
-import LandingPage from './pages/landing';
-import NavBar from './components/navBar';
-import Profile from './pages/profile';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import History from "./pages/history";
+import LandingPage from "./pages/landing";
+import NavBar from "./components/navBar";
+import Profile from "./pages/profile";
+import Privacy from "./pages/privacy";
+import Footer from "./components/footer";
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+  const hideFooter = location.pathname === "/home";
+
   return (
-    <BrowserRouter>
-    <NavBar/>
+    <>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<LandingPage/>}/>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/history" element={<History />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/privacy" element={<Privacy />} />
       </Routes>
+      {!hideFooter && <Footer />}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   );
 }
