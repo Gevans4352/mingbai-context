b07e194ecabeac61eb95d4a90df0df68281e2291
Gevans4352 <emogoghena@gmail.com>
2026-09-05 16:56:28 +0100

    chore: minor back button adjustments

--- src/components/backButton.tsx
+++ src/components/backButton.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 function BackButton() {
   return (
     <Link to="/home" className="back-button">
-      ‹ back to decode
+      <span className="back-arrow">↶</span>
+      <span>back to decode</span>
+      <span className="back-twinkle">✦</span>
     </Link>
   );
 }
